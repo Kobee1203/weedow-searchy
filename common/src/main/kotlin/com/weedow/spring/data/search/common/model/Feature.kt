eab71c3fdceecfd576a2f994a1c1d4e3c4f5400d
Nicolas Dos Santos <kobee1203@users.noreply.github.com>
2020-10-27 15:54:11 +0100

    Update entities to test MapJoin with @MapKey (Vehicle entity with Map<String, Feature>) Update SQL Data

---
+++ common/src/main/kotlin/com/weedow/spring/data/search/common/model/Feature.kt
@@ -0,0 +1,23 @@
+package com.weedow.spring.data.search.common.model
+
+import javax.persistence.*
+
+@Entity
+class Feature(
+        @Column(nullable = false, unique = true)
+        val name: String,
+
+        @Column(nullable = false)
+        val description: String,
+
+        @ElementCollection
+        @CollectionTable(
+                name = "metadata_mapping",
+                joinColumns = [JoinColumn(name = "feature_id", referencedColumnName = "id")])
+        @MapKeyColumn(name = "metadata_name")
+        @Column(name = "value")
+        val metadata: Map<String, String>? = null
+
+) : JpaPersistable<Long>() {
+
+}
