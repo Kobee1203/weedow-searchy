e8d6563d42c6728a856774e5726fcbf3c7aaec1a
Nicolas Dos Santos <kobee1203@users.noreply.github.com>
2020-11-15 14:57:32 +0100

    added new application property 'spring.data.search.default-alias-resolver.field-suffixes': Comma-separated list of field suffixes to be removed in order to create a field's alias from the com.weedow.spring.data.search.config.DefaultAliasResolver. Default value is `Entity,Entities`

--- core/src/main/kotlin/com/weedow/spring/data/search/autoconfigure/DataSearchDefaultAliasConfigurerAutoConfiguration.kt
+++ core/src/main/kotlin/com/weedow/spring/data/search/autoconfigure/DataSearchDefaultAliasConfigurerAutoConfiguration.kt
@@ -3,8 +3,10 @@ package com.weedow.spring.data.search.autoconfigure
 import com.weedow.spring.data.search.alias.AliasResolver
 import com.weedow.spring.data.search.alias.AliasResolverRegistry
 import com.weedow.spring.data.search.config.SearchConfigurer
+import com.weedow.spring.data.search.config.SearchProperties
 import org.apache.commons.lang3.StringUtils
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import java.lang.reflect.Field
 
@@ -13,28 +15,25 @@ import java.lang.reflect.Field
  */
 @Configuration
 @ConditionalOnClass(SearchConfigurer::class)
-class DataSearchDefaultAliasConfigurerAutoConfiguration : SearchConfigurer {
+@EnableConfigurationProperties(SearchProperties::class)
+class DataSearchDefaultAliasConfigurerAutoConfiguration(
+        private val searchProperties: SearchProperties
+) : SearchConfigurer {
 
     override fun addAliasResolvers(registry: AliasResolverRegistry) {
-        registry.addAliasResolver(DefaultAliasResolver())
+        registry.addAliasResolver(DefaultAliasResolver(searchProperties.defaultAliasResolver.fieldSuffixes))
     }
 
     /**
-     * Default Alias Resolver that create a new alias for all fields ending with 'Entity' or 'Entities'.
+     * Default Alias Resolver that creates a new alias for all fields ending with one of the given [fieldSuffixes].
      *
-     * The resolved alias is the field name without 'Entity' and 'Entities'.
+     * The resolved alias is the field name without one of the given [fieldSuffixes].
+     *
+     * @param fieldSuffixes List of field suffixes to be removed in order to create a field's alias
      */
-    class DefaultAliasResolver : AliasResolver {
-
-        companion object {
-            /**
-             * Field suffixes to be removed
-             */
-            private val FIELD_SUFFIXES = listOf(
-                    "Entity",
-                    "Entities"
-            )
-        }
+    class DefaultAliasResolver(
+            private val fieldSuffixes: List<String>
+    ) : AliasResolver {
 
         override fun supports(entityClass: Class<*>, field: Field): Boolean {
             return true
@@ -42,7 +41,7 @@ class DataSearchDefaultAliasConfigurerAutoConfiguration : SearchConfigurer {
 
         override fun resolve(entityClass: Class<*>, field: Field): List<String> {
             val fieldName = field.name
-            for (fieldSuffix in FIELD_SUFFIXES) {
+            for (fieldSuffix in fieldSuffixes) {
                 if (fieldName.endsWith(fieldSuffix)) {
                     return listOf(StringUtils.substringBefore(fieldName, fieldSuffix))
                 }
