87cf39742cc2a2b9960666d0e7b9bd905e5b6ade
Nicolas Dos Santos <kobee1203@users.noreply.github.com>
2021-01-28 19:29:18 +0100

    added MongoPersistable abstract class, that extends org.springframework.data.domain.Persistable, in order to manage automatically the collection id, created date and last modified date

---
+++ mongodb/src/main/kotlin/com/weedow/searchy/mongodb/domain/MongoPersistable.kt
@@ -0,0 +1,73 @@
+package com.weedow.searchy.mongodb.domain
+
+import org.apache.commons.lang3.builder.ToStringBuilder
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.annotation.Transient
+import org.springframework.data.domain.Persistable
+import java.io.Serializable
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
+import java.time.LocalDateTime
+
+abstract class MongoPersistable<ID : Serializable> : Persistable<ID> {
+
+    @Transient
+    @kotlin.jvm.Transient
+    val idClass: Class<ID>
+
+    @Id
+    private var id: ID? = null
+
+    @CreatedDate
+    private var createdOn: LocalDateTime? = null
+
+    @LastModifiedDate
+    private var updatedOn: LocalDateTime? = null
+
+    init {
+        val t: Type = javaClass.genericSuperclass
+        val pt: ParameterizedType = t as ParameterizedType
+        idClass = pt.actualTypeArguments[0] as Class<ID>
+    }
+
+    override fun getId(): ID? {
+        return id
+    }
+
+    open fun setId(id: ID) {
+        this.id = id
+    }
+
+    fun getCreatedOn(): LocalDateTime? {
+        return createdOn
+    }
+
+    fun getUpdatedOn(): LocalDateTime? {
+        return updatedOn
+    }
+
+    @Transient
+    override fun isNew() = null == getId()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MongoPersistable<*>
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+
+    override fun toString(): String {
+        return ToStringBuilder.reflectionToString(this)
+    }
+
+}
