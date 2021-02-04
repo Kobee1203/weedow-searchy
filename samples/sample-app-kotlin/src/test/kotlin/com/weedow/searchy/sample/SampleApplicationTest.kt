2aeddec9938e36753e2a49237ce5aedb026eaf85
Nicolas Dos Santos <kobee1203@users.noreply.github.com>
2021-02-04 17:16:45 +0100

    sample-app-kotlin: add integration tests to test different criteria

--- samples/sample-app-kotlin/src/test/kotlin/com/weedow/searchy/sample/SampleApplicationTest.kt
+++ samples/sample-app-kotlin/src/test/kotlin/com/weedow/searchy/sample/SampleApplicationTest.kt
@@ -1,6 +1,8 @@
 package com.weedow.searchy.sample
 
 import com.weedow.searchy.config.SearchyProperties
+import net.javacrumbs.jsonunit.spring.jsonContent
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
@@ -20,22 +22,165 @@ class SampleApplicationTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
-    @Value("classpath:data/result.json")
-    private lateinit var jsonResult: Resource
-
     @Test
-    fun search() {
+    @DisplayName("GET /search/person")
+    fun search_all(@Value("classpath:data/result_all.json") result: Resource) {
         val basePath = SearchyProperties.DEFAULT_BASE_PATH
 
-        val firstName = "John"
+        val searchyDescriptorId = "person"
+
+        mockMvc.get("$basePath/$searchyDescriptorId")
+            .andExpect {
+                status { isOk() }
+                content { contentType(MediaType.APPLICATION_JSON) }
+                jsonContent { isEqualTo(result.file.readText()) }
+            }
+    }
+
+    @Test
+    @DisplayName("GET /search/person?firstName=John")
+    fun search_from_one_field_as_query_param(@Value("classpath:data/result_john_doe.json") result: Resource) {
+        val basePath = SearchyProperties.DEFAULT_BASE_PATH
+
         val searchyDescriptorId = "person"
 
         mockMvc.get("$basePath/$searchyDescriptorId") {
-            param("firstName", firstName)
+            this.param("firstName", "John")
         }.andExpect {
             status { isOk() }
             content { contentType(MediaType.APPLICATION_JSON) }
-            content { json(jsonResult.file.readText()) }
+            jsonContent { isEqualTo(result.file.readText()) }
+        }
+    }
+
+    @Test
+    @DisplayName("GET /search/person?firstName=John&lastName=Doe")
+    fun search_from_multiple_fields_as_query_param(@Value("classpath:data/result_john_doe.json") result: Resource) {
+        val basePath = SearchyProperties.DEFAULT_BASE_PATH
+
+        val searchyDescriptorId = "person"
+
+        mockMvc.get("$basePath/$searchyDescriptorId") {
+            this.param("firstName", "John")
+            this.param("lastName", "Doe")
+        }.andExpect {
+            status { isOk() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            jsonContent { isEqualTo(result.file.readText()) }
+        }
+    }
+
+    @Test
+    @DisplayName("GET /search/person?firstName=John&firstName=Jane")
+    fun search_from_one_field_as_query_param_with_multiple_values(@Value("classpath:data/result_john_doe_jane_doe.json") result: Resource) {
+        val basePath = SearchyProperties.DEFAULT_BASE_PATH
+
+        val searchyDescriptorId = "person"
+
+        mockMvc.get("$basePath/$searchyDescriptorId") {
+            this.param("firstName", "John")
+            this.param("firstName", "Jane")
+        }.andExpect {
+            status { isOk() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            jsonContent { isEqualTo(result.file.readText()) }
+        }
+    }
+
+    @Test
+    @DisplayName("GET /search/person?birthday=1981-03-12T10:36:00")
+    fun search_from_date_field_as_query_param(@Value("classpath:data/result_john_doe.json") result: Resource) {
+        val basePath = SearchyProperties.DEFAULT_BASE_PATH
+
+        val searchyDescriptorId = "person"
+
+        mockMvc.get("$basePath/$searchyDescriptorId") {
+            this.param("birthday", "1981-03-12T10:36:00")
+        }.andExpect {
+            status { isOk() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            jsonContent { isEqualTo(result.file.readText()) }
+        }
+    }
+
+    @Test
+    @DisplayName("GET /search/person?address.zipCode=78370")
+    fun search_from_nested_string_field_as_query_param(@Value("classpath:data/result_john_doe_jane_doe.json") result: Resource) {
+        val basePath = SearchyProperties.DEFAULT_BASE_PATH
+
+        val searchyDescriptorId = "person"
+
+        mockMvc.get("$basePath/$searchyDescriptorId") {
+            this.param("address.zipCode", "78370")
+        }.andExpect {
+            status { isOk() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            jsonContent { isEqualTo(result.file.readText()) }
+        }
+    }
+
+    @Test
+    @DisplayName("GET /search/person?nickNames=Johnny")
+    fun search_from_nested_string_collection_field_as_query_param(@Value("classpath:data/result_john_doe.json") result: Resource) {
+        val basePath = SearchyProperties.DEFAULT_BASE_PATH
+
+        val searchyDescriptorId = "person"
+
+        mockMvc.get("$basePath/$searchyDescriptorId") {
+            this.param("nickNames", "Johnny")
+        }.andExpect {
+            status { isOk() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            jsonContent { isEqualTo(result.file.readText()) }
+        }
+    }
+
+    @Test
+    @DisplayName("GET /search/person?address.country=FR")
+    fun search_from_nested_enum_field_as_query_param(@Value("classpath:data/result_john_doe_jane_doe.json") result: Resource) {
+        val basePath = SearchyProperties.DEFAULT_BASE_PATH
+
+        val searchyDescriptorId = "person"
+
+        mockMvc.get("$basePath/$searchyDescriptorId") {
+            this.param("address.country", "FR")
+        }.andExpect {
+            status { isOk() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            jsonContent { isEqualTo(result.file.readText()) }
+        }
+    }
+
+    @Test
+    @DisplayName("GET /search/person?address.city=Porticcio")
+    fun search_from_nested_field_as_query_param(@Value("classpath:data/result_john_doe.json") result: Resource) {
+        val basePath = SearchyProperties.DEFAULT_BASE_PATH
+
+        val searchyDescriptorId = "person"
+
+        mockMvc.get("$basePath/$searchyDescriptorId") {
+            this.param("address.city", "Porticcio")
+        }.andExpect {
+            status { isOk() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            jsonContent { isEqualTo(result.file.readText()) }
+        }
+    }
+
+    @Test
+    @DisplayName("GET /search/person?address.city=Porticcio&address.city=Le-Bois-Plage-En-Ré")
+    fun search_from_nested_field_of_object_collection_field_as_query_param_with_multiple_values(@Value("classpath:data/result_john_doe_jane_doe.json") result: Resource) {
+        val basePath = SearchyProperties.DEFAULT_BASE_PATH
+
+        val searchyDescriptorId = "person"
+
+        mockMvc.get("$basePath/$searchyDescriptorId") {
+            this.param("address.city", "Porticcio")
+            this.param("address.city", "Le-Bois-Plage-En-Ré")
+        }.andExpect {
+            status { isOk() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            jsonContent { isEqualTo(result.file.readText()) }
         }
     }
 
