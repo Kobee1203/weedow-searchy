0de9eb404a329a8887d1f0099f523871a7c839d0
Nicolas Dos Santos <kobee1203@users.noreply.github.com>
2021-02-08 20:16:55 +0100

    make constants public to be used in other modules

--- core/src/main/kotlin/com/weedow/searchy/utils/Constants.kt
+++ core/src/main/kotlin/com/weedow/searchy/utils/Constants.kt
@@ -3,14 +3,14 @@ package com.weedow.searchy.utils
 /**
  * Field path separator (eg. vehicle.brand)
  */
-internal const val FIELD_PATH_SEPARATOR = "."
+const val FIELD_PATH_SEPARATOR = "."
 
 /**
  * Special key to be used in the field path to access to the keys of a Map
  */
-internal const val MAP_KEY = "key"
+const val MAP_KEY = "key"
 
 /**
  * Special key to be used in the field path to access to the values of a Map
  */
-internal const val MAP_VALUE = "value"
+const val MAP_VALUE = "value"
